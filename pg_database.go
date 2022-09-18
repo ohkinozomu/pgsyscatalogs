887d1642245f66d833a9c9f347ffff015825f1de
Ohki Nozomu <nozomunoise@gmail.com>
2022-09-18 12:04:28 +0900

    Add pg_database (#1)

---
+++ pg_database.go
@@ -0,0 +1,23 @@
+package pgsyscatalogs
+
+import (
+	"github.com/jackc/pgtype"
+)
+
+// https://www.postgresql.org/docs/current/catalog-pg-database.html
+type pgDatabase struct {
+	OID           pgtype.OID
+	Datname       pgtype.Name
+	Datdba        pgtype.OID
+	Encoding      pgtype.Int4
+	Datcollate    pgtype.Name
+	Datctype      pgtype.Name
+	Datistemplate pgtype.Bool
+	Datallowconn  pgtype.Bool
+	Datconnlimit  pgtype.Int4
+	Datlastsysoid pgtype.OID
+	Datfrozenxid  pgtype.XID
+	Datminmxid    pgtype.XID
+	Dattablespace pgtype.OID
+	Datacl        []pgtype.ACLItem
+}
